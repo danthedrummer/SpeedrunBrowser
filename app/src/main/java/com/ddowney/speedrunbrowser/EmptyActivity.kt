2f60e0dff8d06d2c392d18e1edaa01e9e701fe69
danthedrummer <danthedrummer@gmail.com>
2022-05-31 22:01:19 +0100

    Experimenting with compose

--- app/src/main/java/com/ddowney/speedrunbrowser/EmptyActivity.kt
+++ app/src/main/java/com/ddowney/speedrunbrowser/EmptyActivity.kt
@@ -1,15 +1,58 @@
 package com.ddowney.speedrunbrowser
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ListItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.ddowney.speedrunbrowser.core.network.api.GameApi
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
 /**
  * As it says on the tin. Just an empty activity for the app to
  * launch into for now. Will be removed later.
  */
+@AndroidEntryPoint
 class EmptyActivity : AppCompatActivity() {
+
+  @Inject
+  lateinit var gameApi: GameApi
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_empty)
+    lifecycleScope.launch(Dispatchers.IO) {
+      delay(5_000L)
+      val games = gameApi.getGames()
+      withContext(Dispatchers.Main) {
+        setContent {
+          GameList(games = games)
+        }
+      }
+    }
+    setContent {
+      Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize(),
+      ) {
+        CircularProgressIndicator()
+      }
+    }
   }
 }
